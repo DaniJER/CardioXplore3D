82d1315929aba57f07482ad0b02122ae6ff71623
Daniel <danicraft.op7@gmail.com>
2025-04-27 21:15:36 -0500

    feat: add about us page. fix: extend home banner.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from './pages/home/home';
-import Quiz from './pages/quiz/quiz';
-import Diseases from './pages/diseases/diseases';
-import Profile from './pages/profile/profile';
-import Login from './pages/login/login';
-import NotFound from './pages/not-found/NotFound';
-import Header from './layout/header/Header';
-import Footer from './layout/footer/Footer';
-import ProtectedRoute from './utils/ProtectedRoute';
-import DiseaseDetail2 from './pages/diseases/Details/DiseaseDetail2';
-import DiseaseDetail3 from './pages/diseases/Details/DiseaseDetail3';
-import DiseaseDetail4 from './pages/diseases/Details/DiseaseDetail4';
-import PageHA from './pages/diseases/HA/PageHA';
+import Home from "./pages/home/home";
+import Quiz from "./pages/quiz/quiz";
+import Diseases from "./pages/diseases/diseases";
+import Profile from "./pages/profile/profile";
+import Login from "./pages/login/login";
+import AboutUs from "./pages/aboutUS/aboutUs";
+import NotFound from "./pages/not-found/NotFound";
+import Header from "./layout/header/Header";
+import Footer from "./layout/footer/Footer";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import DiseaseDetail2 from "./pages/diseases/Details/DiseaseDetail2";
+import DiseaseDetail3 from "./pages/diseases/Details/DiseaseDetail3";
+import DiseaseDetail4 from "./pages/diseases/Details/DiseaseDetail4";
+import PageHA from "./pages/diseases/HA/PageHA";
 
 function App() {
-
   return (
     <Router>
       <Header />
@@ -23,16 +23,20 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/enfermedades" element={<Diseases />} />
-        <Route path='/enfermedades/1' element={<PageHA />} />
-        <Route path='/enfermedades/2' element={<DiseaseDetail2 />} />
-        <Route path='/enfermedades/3' element={<DiseaseDetail3 />} />
-        <Route path='/enfermedades/4' element={<DiseaseDetail4 />} />
-        <Route path="/profile" element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        } />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/nosotros" element={<AboutUs />} />
+        <Route path="/enfermedades/1" element={<PageHA />} />
+        <Route path="/enfermedades/2" element={<DiseaseDetail2 />} />
+        <Route path="/enfermedades/3" element={<DiseaseDetail3 />} />
+        <Route path="/enfermedades/4" element={<DiseaseDetail4 />} />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
