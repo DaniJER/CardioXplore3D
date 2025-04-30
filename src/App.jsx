cbbbc16dd3ac1da18cdcd14fb4a7e9b42fd6a7ef
PacmanLloron <115446085+PacmanLloron@users.noreply.github.com>
2025-04-30 04:02:44 -0500

    Correcion de errores en de sintaxis en el archivo App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/home";
-import Quiz from "./pages/quiz/quiz";
-import Diseases from "./pages/diseases/diseases";
-import Profile from "./pages/profile/profile";
-import Login from "./pages/login/login";
-import AboutUs from "./pages/aboutUs/AboutUs";
-import NotFound from "./pages/not-found/NotFound";
-import Header from "./layout/header/Header";
-import Footer from "./layout/footer/Footer";
-import ProtectedRoute from "./utils/ProtectedRoute";
-import PageHA from "./pages/diseases/HA/PageHA";
-import PageEC from "./pages/diseases/EC/PageEC";
-import PageEAC from "./pages/diseases/EAC/PageEAC";
-import PageIC from "./pages/diseases/IC/PageIC";
+import Home from './pages/home/home';
+import Quiz from './pages/quiz/quiz';
+import Diseases from './pages/diseases/diseases';
+import Profile from './pages/profile/profile';
+import Login from './pages/login/login';
+import CreateUser from './pages/create-user/CreateUser';
+import AboutUs from "./pages/aboutUS/aboutUs";
+import NotFound from './pages/not-found/NotFound';
+import Header from './layout/header/Header';
+import Footer from './layout/footer/Footer';
+import ProtectedRoute from './utils/ProtectedRoute';
+import PageHA from './pages/diseases/HA/PageHA';
+import PageEC from './pages/diseases/EC/PageEC';
+import PageEAC from './pages/diseases/EAC/PageEAC';
+import PageIC from './pages/diseases/IC/PageIC';
 
 function App() {
   return (
@@ -24,18 +25,15 @@ function App() {
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/enfermedades" element={<Diseases />} />
         <Route path="/nosotros" element={<AboutUs />} />
-        <Route path="/enfermedades/1" element={<PageHA />} />
+        <Route path='/enfermedades/1' element={<PageHA />} />
         <Route path="/enfermedades/2" element={<PageEC />} />
         <Route path="/enfermedades/3" element={<PageEAC />} />
         <Route path="/enfermedades/4" element={<PageIC />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        } />
         <Route path="*" element={<NotFound />} />
         <Route path="/login" element={<Login />} />
         <Route path="/create-user" element={<CreateUser />} />
