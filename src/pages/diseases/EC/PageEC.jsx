55c40cbdd5433c4b5625fc2a2000eae81df5e0c0
PacmanLloron <115446085+PacmanLloron@users.noreply.github.com>
2025-05-27 23:16:31 -0500

    Cambios y modelo nuevo 3D

--- src/pages/diseases/EC/PageEC.jsx
+++ src/pages/diseases/EC/PageEC.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-// import Prevention from "../sections/Prevention";
-// import Symptoms from "../sections/Symptoms";
-// import Treatments from "../sections/Treatments";
+import Prevention from "../sections/Prevention";
+import Symptoms from "../sections/Symptoms";
+import Treatments from "../sections/Treatments";
 import WhatIs from "../sections/WhatIs";
 import "./PageEC.css"
 
 import { Heart } from "./models-3d/Heart";
+import { Surgeon } from "./models-3d/Surgeon";
 
 
 const PageEC = () => {
@@ -14,9 +15,11 @@ const PageEC = () => {
         <WhatIs
           title="Comunicación interventricular"
           subtitle="Defecto del tabique ventricular"
-          description="La comunicación interventricular consiste en un orificio en el corazón. Es un problema cardíaco común presente al nacer (defecto cardíaco congénito). El orificio aparece en la pared que separa las cavidades inferiores del corazón (ventrículos)."
+          description="La comunicación interventricular (CIV) es un tipo de cardiopatía congénita, es decir, una malformación del corazón presente desde el nacimiento. Consiste en la existencia de un orificio anormal en el tabique interventricular, la pared muscular que separa los dos ventrículos (cavidades inferiores del corazón).
+          
+          Este orificio permite que la sangre rica en oxígeno que debería salir hacia el cuerpo desde el ventrículo izquierdo se mezcle con sangre pobre en oxígeno del ventrículo derecho. Como resultado, una parte de la sangre vuelve a circular hacia los pulmones innecesariamente, lo que sobrecarga tanto al corazón como al sistema pulmonar."
             Model3D={Heart}
-            scale={5.45}
+            scale={4.44}
             position={[0, 0, 0]}
             rotation={[0, 0, 0]}
             rotationSpeed={0.01}
@@ -25,6 +28,32 @@ const PageEC = () => {
             planoRotacion={[-Math.PI / 2, 0, 0]}
             planoEscala={[30, 30]}
         />
+        <Symptoms
+        title="Causas, diagnóstico y Síntomas"
+        description="La CIV puede presentarse de forma aislada o como parte de otras malformaciones cardíacas más complejas. Suele detectarse mediante ecocardiografía (ultrasonido del corazón), especialmente si el médico escucha un soplo cardíaco en el examen físico."
+        lastDescription={"Los síntomas pueden variar según el tamaño del defecto y la cantidad de sangre que fluye a través de él. En algunos casos, los bebés pueden no presentar síntomas evidentes, mientras que en otros pueden experimentar dificultad para respirar, fatiga, retraso en el crecimiento y otros problemas relacionados con la circulación sanguínea."}
+        items={[
+          "Dificultad para respirar.",
+
+          "Fatiga o sudoración al alimentarse (en bebés).",
+
+          "Infecciones respiratorias frecuentes.",
+          
+          "Crecimiento deficiente.",
+          
+          "Soplo cardíaco audible con estetoscopio."
+        ]}
+        // Modelo 3D
+        Model3D={Surgeon}
+        scale={1.45}
+        position={[0, -2.2, 0]}
+        rotation={[0, 0, 0]}
+        rotationSpeed={0.01}
+        mostrarPlano={false}
+        planoPosicion={[0, -2.8, 0]}
+        planoRotacion={[-Math.PI / 2, 0, 0]}
+        planoEscala={[30, 30]}
+      />
       
       </div>
     );
