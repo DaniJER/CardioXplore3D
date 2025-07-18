650de09b5d722e7f385434db1d65df748ebdf96c
PacmanLloron <115446085+PacmanLloron@users.noreply.github.com>
2025-07-18 01:54:27 -0500

    Correccion de modelados y aplicacion de texturas

--- src/pages/diseases/EC/models-3d/Heart.jsx
+++ src/pages/diseases/EC/models-3d/Heart.jsx
@@ -6,38 +6,14 @@ export function Heart(props) {
   const { nodes, materials } = useGLTF('/models-3d/EC/modelado-de-corazon.glb')
     console.log(materials)
   return (
-    <group {...props} dispose={null}>
+     <group {...props} dispose={null}>
       <mesh
         castShadow
         receiveShadow
         geometry={nodes.Heart.geometry}
-        material={materials.Materialheart}
+        material={materials['Material.001']}
         rotation={[0.013, 0, 0]}
       />
-      {/* <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Icosphere.geometry}
-        material={nodes.Icosphere.material}
-        position={[0.086, 0, 0]}
-        scale={0.075}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Icosphere001.geometry}
-        material={nodes.Icosphere001.material}
-        position={[-0.091, -0.099, 0.101]}
-        scale={0.06}
-      /> */}
-      {/* <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Heart_Domain.geometry}
-        material={materials.Material}
-        position={[0, -0.018, 0]}
-        scale={[0.345, 0.767, 0.285]}
-      /> */}
     </group>
   )
 }
